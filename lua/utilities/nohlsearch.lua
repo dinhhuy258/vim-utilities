e0ba1849d9eb791787fed86a210715c9bb28d8b8
Huy Duong <huy.duongdinh@gmail.com>
2022-04-30 22:06:45 +0700

    Fix nohlsearch

--- lua/utilities/nohlsearch.lua
+++ lua/utilities/nohlsearch.lua
@@ -1,30 +1,32 @@
 local M = {}
 
 function M.stop_hlsearch()
-  if vim.v.hlsearch == 1 and vim.fn.mode() == "n" then
+  if vim.v.hlsearch == 1 and vim.fn.mode() == "n" and vim.api.nvim_win_get_config(0)['relative'] == "" then
     vim.fn.feedkeys "<vim_utilities>(stop_hightlight_search)"
   end
-end
-
-function M.start_hlsearch()
-  vim.cmd [[
-    if v:hlsearch && !search('\%#\zs'.@/,'cnW')
-      call luaeval("require'utilities.nohlsearch'.stop_hlsearch()")
-    endif
-  ]]
 end
 
 function M.setup()
   vim.cmd "noremap <silent> <vim_utilities>(stop_hightlight_search) :<C-U>nohlsearch<CR>"
   vim.cmd "noremap! <expr> <vim_utilities>(stop_hightlight_search) execute('nohlsearch')[-1]"
 
-  vim.cmd "augroup vim_utilities_nohlsearch"
-  vim.cmd "autocmd!"
+  vim.api.nvim_create_autocmd("CursorMoved", {
+    pattern = "*",
+    callback = function(args)
+      vim.cmd [[
+        if v:hlsearch && !search('\%#\zs'.@/,'cnW')
+          call luaeval("require'utilities.nohlsearch'.stop_hlsearch()")
+        endif
+      ]]
+    end,
+  })
 
-  vim.cmd "autocmd CursorMoved * call luaeval(\"require'utilities.nohlsearch'.start_hlsearch()\")"
-  vim.cmd "autocmd InsertEnter * call luaeval(\"require'utilities.nohlsearch'.stop_hlsearch()\")"
-
-  vim.cmd "augroup END"
+  vim.api.nvim_create_autocmd("InsertEnter", {
+    pattern = "*",
+    callback = function(args)
+      require'utilities.nohlsearch'.stop_hlsearch()
+    end,
+  })
 end
 
 return M
